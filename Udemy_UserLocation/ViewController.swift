c803c7f9ac1e881855b63a85da638619f47aa5d3
Abdulhakim <you@yourdomain.com>
2016-09-13 04:38:25 +0100

    Initial Commit

---
+++ Udemy_UserLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udemy_UserLocation
+//
+//  Created by Abdulhakim Ajetunmobi on 13/09/2016.
+//  Copyright © 2016 5to9 Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
